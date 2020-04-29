f82873849d11171ab3d25e5250a26a1183add65d
rebornman <fm_reborn@hotmail.com>
2020-04-29 20:46:00 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WebsiteDeadPageFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.URL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timeOut = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timeOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 94);
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(1033, 408);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(955, 508);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(90, 33);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(161, 32);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(884, 20);
+            this.URL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Website Sitemap Location:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(859, 508);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(90, 33);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // timeOut
+            // 
+            this.timeOut.Location = new System.Drawing.Point(307, 59);
+            this.timeOut.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.timeOut.Name = "timeOut";
+            this.timeOut.Size = new System.Drawing.Size(52, 20);
+            this.timeOut.TabIndex = 5;
+            this.timeOut.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Timeout in seconds before determining dead page or error:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 553);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timeOut);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.URL);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.Output);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Website Dead Page Finder v1.01";
+            ((System.ComponentModel.ISupportInitialize)(this.timeOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.NumericUpDown timeOut;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WebsiteDeadPageFinder
+{
+    public partial class Form1 : Form
+    {
+        string Title = "Website Dead Page Finder";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(URL.Text, UriKind.Absolute))
+            {
+                LogIt("======================================== URLS ============================================");
+                List<string> urls = GetUrlsFromSiteMap(URL.Text);
+                LogIt("==========================================================================================");
+                int counter = 0;
+                foreach (string page in urls)
+                {
+                    bool result = DoesSiteExist2(page);
+                    if(result == true)
+                    {
+                        LogIt("Page " + page + " does exist!");
+                    }
+                    else
+                    {
+                        LogIt("Warning!!!");
+                        LogIt(page + " DOES NOT EXIST OR HAS A SERIOUS ERROR!");
+                        LogIt("Warning!!!");
+                        counter++;
+                    }
+                }
+                LogIt("=======================================================================================================");
+                LogIt("I found " + counter.ToString() + " pages that did not exist (or had a significant error), but where in the sitemap.");
+                LogIt("=======================================================================================================");
+                if (counter > 0)
+                {
+                    LogIt("Pages that are listed in the sitemap but do not actually exist are bad. Google crawls these pages and finds zero content! It affects your search engine rank!");
+                    LogIt("==============================================================================================================================================================");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You need to enter a sitemap address.", Title);
+            }
+        }
+
+
+
+        bool DoesSiteExist(string url)
+        {
+            bool result = true;
+            try
+            {
+                Uri uri = new Uri(url);
+                WebRequest request = WebRequest.Create(uri);
+                request.Timeout = (int)timeOut.Value * 1000;
+                WebResponse response;
+                try
+                {
+                    response = request.GetResponse();
+                }
+                catch (Exception ex)
+                {
+                    result = false;
+                    LogIt("Error: " + ex.Message.ToString() + " for page: " + url);
+                    //MessageBox.Show("The website entered doesn't exist.", Title);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to check if site exists.\nError Code: " + ex.Message.ToString(), Title);
+                LogIt("Error: " + ex.Message.ToString());
+            }
+            return result;
+        }
+
+
+        bool DoesSiteExist2(string url)
+        {
+            bool result = true;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Timeout = (int)timeOut.Value * 1000;
+                request.AllowAutoRedirect = false;
+                request.Method = "HEAD";
+
+                using (var response = request.GetResponse())
+                {
+                    result = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                result = false;
+                LogIt("Error: " + ex.Message.ToString() + " for page: " + url);
+            }
+            return result;
+        }
+
+
+        List<string> GetUrlsFromSiteMap(string SitemapURL)
+        {
+            LogIt("Attempting to retrieve web pages and their urls from the website's sitemap...\n");
+            List<string> urls = new List<string>();
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.Encoding = System.Text.Encoding.UTF8;
+                string reply = wc.DownloadString(SitemapURL);
+                XmlDocument urldoc = new XmlDocument();
+                urldoc.LoadXml(reply);
+                XmlNodeList xnList = urldoc.GetElementsByTagName("url");
+
+                foreach (XmlNode node in xnList)
+                {
+                    LogIt("url: " + node["loc"].InnerText);
+                    urls.Add(node["loc"].InnerText);
+                }
+                Output.Text = Output.Text + "\n\n";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("This is not a valid sitemap address, the author of the website hasn't created it properly. You will need to manually visit each page.", Title);
+                LogIt("This is not a valid sitemap address, the author of the website hasn't created it properly. You will need to manually visit each page.");
+                LogIt("Error: " + ex.Message.ToString());
+            }
+            return urls;
+        }
+
+
+
+        void LogIt(string message)
+        {
+            Output.Text = Output.Text + DateTime.Now.ToString() + ":   " + message + "\n";
+            Output.Focus();
+            Output.SelectionStart = Output.Text.Length;
+            Output.ScrollToCaret();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Output.Text = "";
+        }
+    }
+}
